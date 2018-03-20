66c3edb76c4373660f74ef976dfa291b79a3a2ce
thol-voleak <voleak.rupp@gmail.com>
2018-03-20 09:38:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World From Voleak Thol</p>'; ?> 
+ <?php 
+   echo '<p>Hello World From Voleak Thol</p>'; 
+   echo getenv("POSTGRESQL_SERVICE_HOST");
+   echo getenv("POSTGRESQL_SERVICE_PORT");
+   echo getenv("dbname");
+   echo getenv("dbuser");
+   echo getenv("dbpassword");
+  ?> 
  </body>
 </html>
