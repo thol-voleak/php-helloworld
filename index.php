6c136b8559ccc6df6bda209ea166e9b3624ce9e8
thol-voleak <voleak.rupp@gmail.com>
2018-03-20 09:58:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,17 @@
  </head>
  <body>
  <?php 
-   echo '<p>Hello World From Voleak Thol</p>'; 
-   echo "Host: {$getenv("POSTGRESQL_SERVICE_HOST")} ";
-   echo "Port: {getenv("POSTGRESQL_SERVICE_PORT")} ";
-   echo "DB Name: {getenv("dbname")} ";
-   echo "User: {getenv("dbuser")} ";
-   echo "Password: {getenv("dbpassword")} ";
+   $POSTGRESQL_SERVICE_HOST = getenv("POSTGRESQL_SERVICE_HOST");
+   $POSTGRESQL_SERVICE_PORT = getenv("POSTGRESQL_SERVICE_PORT");
+   $dbname = getenv("dbname");
+   $dbuser = getenv("dbuser");
+   $dbpassword = getenv("dbpassword");
+  
+   echo "Host: {$POSTGRESQL_SERVICE_HOST"} ";
+   echo "Port: {$POSTGRESQL_SERVICE_PORT"} ";
+   echo "DB Name: {$dbname} ";
+   echo "User: {$dbuser} ";
+   echo "Password: {$dbpassword} ";
   ?> 
  </body>
 </html>
