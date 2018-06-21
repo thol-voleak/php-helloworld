526bd3473c1f314fa91a0e135504506e064ce8b2
thol-voleak <voleak.rupp@gmail.com>
2018-06-21 09:14:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  <body>
  <?php 
    $host = gethostname();
-   echo "blue/green v3.0 server host: {$host} "
+   echo "hello world form server host: {$host} "
    /*$POSTGRESQL_SERVICE_HOST = getenv("POSTGRESQL_SERVICE_HOST");
    $POSTGRESQL_SERVICE_PORT = getenv("POSTGRESQL_SERVICE_PORT");
    $dbname = getenv("dbname");
