b2e8ce8efb4132c3b4060cac41bbe2dce8bdc7f5
thol-voleak <voleak.rupp@gmail.com>
2018-07-04 14:01:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
  <?php 
    $host = gethostname();
    echo "hello world form server host: {$host} "
+   for ($x = 0; $x <= 100; $x++) {
+    echo "The number is: $x <br>";
+   } 
    /*$POSTGRESQL_SERVICE_HOST = getenv("POSTGRESQL_SERVICE_HOST");
    $POSTGRESQL_SERVICE_PORT = getenv("POSTGRESQL_SERVICE_PORT");
    $dbname = getenv("dbname");
