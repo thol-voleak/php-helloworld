a42a553a2ac80249f8da5e388e5251c822f953b8
thol-voleak <voleak.rupp@gmail.com>
2018-07-05 09:16:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,42 +4,13 @@
  </head>
  <body>
  <?php
-  class AsyncOperation extends Thread {
-
-    public function __construct($arg) {
-        $this->arg = $arg;
-    }
-
-    public function run() {
-        if ($this->arg) {
-            $sleep = mt_rand(1, 10);
-            printf('%s: %s  -start -sleeps %d' . "\n", date("g:i:sa"), $this->arg, $sleep);
-            sleep($sleep);
-            printf('%s: %s  -finish' . "\n", date("g:i:sa"), $this->arg);
-        }
-    }
-}
-
-// Create a array
-$stack = array();
-
-//Initiate Multiple Thread
-foreach ( range("A", "D") as $i ) {
-    $stack[] = new AsyncOperation($i);
-}
-
-// Start The Threads
-foreach ( $stack as $t ) {
-    $t->start();
-}
-
-  /*
+ 
    $host = gethostname();
    echo "hello world form server host: {$host} ";
-   for ($x = 0; $x <= 1000; $x++) {
+   for ($x = 0; $x <= 10000; $x++) {
     echo "The number is: $x <br>";
    }
-   */
+
    /*$POSTGRESQL_SERVICE_HOST = getenv("POSTGRESQL_SERVICE_HOST");
    $POSTGRESQL_SERVICE_PORT = getenv("POSTGRESQL_SERVICE_PORT");
    $dbname = getenv("dbname");
